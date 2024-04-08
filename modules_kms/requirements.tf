2f06edfe1749918ada5706eb5103748a4d6edce3
Ubuntu <azureuser@Terraform-machine.gzvdlwwrvjpuzcxbgpmgw4w0ma.bx.internal.cloudapp.net>
2024-04-08 04:16:00 +0000

    modifing module_kms

--- modules_kms/requirements.tf
+++ modules_kms/requirements.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     aws = {
-        source = "hasicorp/aws"
+        source = "hashicorp/aws"
         version = ">= 3.15"
     }
   }
